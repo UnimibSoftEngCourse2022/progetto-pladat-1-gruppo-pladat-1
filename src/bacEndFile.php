97dd9cca527c4012f9231307da4bd866d2d5e8ae
Gabriele Lecchi <g.lecchi6@campus.unimib.it>
2022-02-08 12:00:36 +0100

    Created database connection and queries
    Set up database connection and created multiple queries for insertion of data for multiple tables.

--- src/bacEndFile.php
+++ src/bacEndFile.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dbname = "mydb";
 $username = "root";
-$passworddb = "Polk55_5";
+$passworddb = "dbpass";
 
 #campi vari rene
 $emailStudent = "rene@prova.it";
@@ -11,6 +11,7 @@ $nome = "rene";
 $cognome = "jorge";
 $dataNascita = "2000-03-03";
 $passwordStudent = "RR12345678";
+$idPlacement = 4;
 
 
 try{
@@ -22,40 +23,66 @@ try{
     die();
 }
 
+$studentemail = 'prova2@email.com';
+$employeremail = 'microsoft@gmial.com';
+$category = 'Java';
+$idPhoto = 1;
 #inserimento student nel db + check presenza mail
-
-    $db.query("insert into student(email, name, surname, birth_date, presentation, password)
-	values ('prova1@email.com', 'mario', 'giordano', '1966-10-21', 'sono un pagliaccio', 'z1234');");
-
-/*
-##set categorie per studente
-
-    $db.query("insert into student_has_category (Student_email, Category_Name)
-	values('', '');");
-
-###set immagine profilo per student + controllo su photo email studente
-
-    $db.query("");
-
+if(($db->query("SELECT email FROM student WHERE email = '$studentemail';")->fetch()['email'] == null)
+    && ($db->query("SELECT email FROM employer WHERE email = '$employeremail';")->fetch()['email'] == null)){
+    $db->query("insert into student(email, name, surname, birth_date, presentation, password)
+	values ('$studentemail', 'mario', 'giordano', '1966-10-21', 'sono un pagliaccio', 'z1234');");
+}
 #inserimento employer nel db + check presenza email
+if(($db->query("SELECT email FROM employer WHERE email = '$employeremail';")->fetch()['email'] == null)
+    && ($db->query("SELECT email FROM student WHERE email = '$studentemail';")->fetch()['email'] == null)){
+    $db->query("insert into mydb.employer(email, name, description, address, password)
+        values ('$employeremail', 'microsoft', 'microsoft descreiption', 'microsoft addr', 'onedrive1');");
+}
 
-    $db.query("insert into mydb.employer(email, name, description, address, password)
-    values ('', '', '', '', '');");
+##set categorie per studente
+if(($db->query("SELECT email FROM student WHERE email = '$studentemail';")->fetch()['email'] != null)
+    && ($db->query("SELECT Student_email FROM student_has_category WHERE Student_email = '$studentemail' AND Category_Name = '$category';")->fetch()['Student_email'] == null)){
+    $db->query("insert into student_has_category (Student_email, Category_Name)
+	values('$studentemail', '$category');");
+}
 
-##set immagine profilo per employer + controllo su photo email employer
-
-    $db.query("");
+###creazione photo student
+if(($db->query("SELECT email FROM student WHERE email = '$studentemail';")->fetch()['email'] != null)){
+    $db->query("insert into photo (description, Path, student_email)
+	values('description photo', 'path', '$studentemail');");
+}
+$pathphoto = '';
+###get path immagine profilo di student + controllo su photo email studente
+if(($db->query("SELECT email FROM student WHERE email = '$studentemail';")->fetch()['email'] != null)
+    && ($db->query("SELECT idPhoto FROM photo WHERE idPhoto = '$idPhoto';")->fetch()['idPhoto'] != null)){
+    $pathphoto = $db->query("select Path from photo where student_email = '$studentemail'")->fetch()['Path'];
+}
+###creazione photo employer
+if(($db->query("SELECT email FROM employer WHERE email = '$employeremail';")->fetch()['email'] != null)){
+    $db->query("insert into photo (description, Path, employer_email)
+	values('description photo empl', 'path empl', '$employeremail');");
+}
+$pathphoto = '';
+###get path immagine profilo di employer + controllo su photo email employer
+if(($db->query("SELECT email FROM employer WHERE email = '$employeremail';")->fetch()['email'] != null)
+    && ($db->query("SELECT idPhoto FROM photo WHERE idPhoto = '$idPhoto';")->fetch()['idPhoto'] != null)){
+    $pathphoto = $db->query("select Path from photo where employer_email = '$employeremail'")->fetch()['Path'];
+}
 
 #inserimento placement nel db
-
-    $db.query("insert into mydb.placement(title, description, duration, start_date, expiration_date, start_placement, salary, employer_email)
-    values ('','','','','','','','');");  
+if($db->query("SELECT email FROM employer WHERE email = '$employeremail';")->fetch()['email'] != null){
+    $db->query("insert into mydb.placement(title, description, duration, start_date, expiration_date, start_placement, salary, employer_email)
+    values ('titolonew','desc new','10','2000-12-12','2001-12-12','2002-12-12','0','$employeremail');");
+}
 
 ##set categorie per placement
+if($db->query("SELECT idPlacement FROM placement WHERE idPlacement = '$idPlacement';")->fetch()['idPlacement'] != null) {
+    $db->query("insert into placement_has_category(Placement_idPlacement, Category_Name)
+	values ('$idPlacement', '$category');");
+}
 
-    $db.query("insert into placement_has_category(Placement_idPlacement, Category_Name)
-	values ('id', '');");
-
+/*
 ###set immagine profilo per placement
 
     $db.query("insert into mydb.placement_has_photo (Placement_idPlacement, Photo_idPhoto)
