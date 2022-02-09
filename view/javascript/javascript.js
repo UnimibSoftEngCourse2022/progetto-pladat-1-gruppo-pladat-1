5dfd423612bc37bcfd36f16edf41829db045c767
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2022-02-09 10:45:06 +0100

    assegnazione logo

--- view/javascript/javascript.js
+++ view/javascript/javascript.js
@@ -1,24 +1,20 @@
 $(document).ready(function () {
-    setInterval(myfunct,2000);
-  });
-let count=1;
-function myfunct()
-{
-        if(count==0)
-        {
-            $("#motivatore").html("Semplice");
-        }
-        else if(count ==1)
-        {
-           $("#motivatore").html("Porfessionale");
-        }
-        else
-        {
-                $("#motivatore").html("Adatto a te");
-        }
-        count++;
-        count=count%3;
-        $("#motivatore").removeClass("fadeIn");
-        $("#motivatore").width();
-        $("#motivatore").addClass("fadeIn");
+	setInterval(myfunct, 2000);
+});
+let count = 1;
+
+function myfunct() {
+	if (count == 0) {
+		$("#motivatore").html("Semplice");
+	} else if (count == 1) {
+		$("#motivatore").html("Porfessionale");
+	} else {
+		$("#motivatore").html("Adatto a te");
+	}
+	count++;
+	count = count % 3;
+	$("#motivatore").removeClass("fadeIn");
+	$("#motivatore").width();
+	$("#motivatore").addClass("fadeIn");
 }
+
