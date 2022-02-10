4fa0107ef25c55026b6ae4c70abe9a0e83e970a8
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2022-02-11 00:16:03 +0100

    pulizia generale

--- view/javascript/javascript.js
+++ view/javascript/javascript.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 	setInterval(myfunct, 2000);
-	$( ".elenco-privato" ).css('height', ($( "#iscrizione" ).parent().height()-10)+"px");
-	$( ".elenco-privato >div" ).css('height', ($( ".elenco-privato >div" ).parent().height()-$( ".elenco-privato >div" ).parent().children("h3").height()-10)+"px");
+	$(".elenco-privato").css('height', ($("#iscrizione").parent().height() - 10) + "px");
 	$(".gruppo>input, .gruppo>textarea").val("");
 	$(".gruppo>input[type=date]").val(new Date().toISOString().split('T')[0]);
 	$(".gruppo>input, .gruppo>textarea").attr("");
@@ -24,26 +23,20 @@ function myfunct() {
 	$("#motivatore").addClass("fadeIn");
 }
 
-function blurr()
-{
-	$(".gruppo > input[type=date]").focus(function()
-	{
-		$(this).css("color","#3c4043");
+function blurr() {
+	$(".gruppo > input[type=date]").focus(function () {
+		$(this).css("color", "#3c4043");
 	});
-	$(".gruppo > input, .gruppo>textarea").focus(function()
-	{
+	$(".gruppo > input, .gruppo>textarea").focus(function () {
 		$(this).parent().addClass("gruppo-evidenziato");
 	});
-	$(".gruppo > input, .gruppo>textarea").focusout(function()
-	{
-		if($(this).val().trim()==="")
-		{
+	$(".gruppo > input, .gruppo>textarea").focusout(function () {
+		if ($(this).val().trim() === "") {
 			$(this).parent().removeClass("gruppo-evidenziato");
 			$(this).val("");
 		}
 	});
-	$(".gruppo > label").click(function()
-	{
+	$(".gruppo > label").click(function () {
 		$(this).parent().addClass("gruppo-evidenziato");
 		$(this).parent().children("input").focus();
 	});
