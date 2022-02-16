e5ff09cd73d7f71c8a4da4e5fcf19006609c0bbc
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2022-02-16 06:30:10 +0100

    finalizzazione/validazione/pulizia js e html ricerca

--- view/javascript/ricerca.js
+++ view/javascript/ricerca.js
@@ -6,80 +6,76 @@ $(document).ready(function () {
 	elencoOfferte();
 })
 
-function animazioneRicerca()
-{
+function animazioneRicerca() {
 	const chi = document.getElementById('scrittaRicerconaAuto');
-	let istanza=new TypeIt(chi, {
+	let istanza = new TypeIt(chi, {
 		strings: "Ricerca il tirocinio adatto a te",
 		speed: 100,
-  		startDelay: 1000,
-		loop:true,
-		loopDelay:5000,
-	  }).go();
-	$("#ricercona").focus(function() {
+		startDelay: 1000,
+		loop: true,
+		loopDelay: 5000,
+	}).go();
+	$("#ricercona").focus(function () {
 		istanza.freeze();
 		$('#scrittaRicerconaAuto').addClass("nascondi");
 	})
-	$("#ricercona").focusout(function() {
-		if($("#ricercona").val().trim()==="")
-		{
+	$("#ricercona").focusout(function () {
+		if ($("#ricercona").val().trim() === "") {
 			istanza.unfreeze()
 			$('#scrittaRicerconaAuto').removeClass("nascondi");
 		}
 	})
 }
-let lista=["ciao","pippo"];
-function caricaRicerca()
-{
-	$('#elencoRicerca').css("display","none");
-	$('#ricercona').focus(function(){
-		$('#elencoRicerca').css("display","block");
-		$('#ricercona').css("border-top","2px solid #f2f2f2");
-		$('#ricercona').css("border-left","2px solid #f2f2f2");
-		$('#ricercona').css("border-right","2px solid #f2f2f2");
-		$('#ricercona').css("border-bottom","0");
+let lista = ["ciao", "pippo"];
+
+function caricaRicerca() {
+	$('#elencoRicerca').css("display", "none");
+	$('#ricercona').focus(function () {
+		$('#elencoRicerca').css("display", "block");
+		$('#ricercona').css("border-top", "2px solid #f2f2f2");
+		$('#ricercona').css("border-left", "2px solid #f2f2f2");
+		$('#ricercona').css("border-right", "2px solid #f2f2f2");
+		$('#ricercona').css("border-bottom", "0");
 	})
-	$('#ricercona').focusout(function(){
-		$('#elencoRicerca').css("display","none");
-		$('#ricercona').css("border-top","0");
-		$('#ricercona').css("border-left","0");
-		$('#ricercona').css("border-right","0");
-		if($('#ricercona').val().trim()!=="")
-		{
-			$('#ricercona').css("border-bottom","2px solid #1a73e8");
-		}
-		else
-		{
-			$('#ricercona').css("border-bottom","2px solid #f2f2f2");
+	$('#ricercona').focusout(function () {
+		$('#elencoRicerca').css("display", "none");
+		$('#ricercona').css("border-top", "0");
+		$('#ricercona').css("border-left", "0");
+		$('#ricercona').css("border-right", "0");
+		if ($('#ricercona').val().trim() !== "") {
+			$('#ricercona').css("border-bottom", "2px solid #1a73e8");
+		} else {
+			$('#ricercona').css("border-bottom", "2px solid #f2f2f2");
 		}
 	})
-	$('#elencoRicerca').hover(function() {
+	$('#elencoRicerca').hover(function () {
 		$('#ricercona').focus();
 	});
 	for (let i = 0; i < lista.length; i++) {
-		$('#elencoRicerca').append("<li onclick='scriviricerca(this)'>"+lista[i]+"</li>");
+		$('#elencoRicerca').append("<li onclick='scriviricerca(this)'>" + lista[i] + "</li>");
 	}
-	$("#ricercona").keyup(function(){
+	$("#ricercona").keyup(function () {
 		$('#elencoRicerca').text("");
 		for (let i = 0; i < lista.length; i++) {
 			if (lista[i].toUpperCase().indexOf($("#ricercona").val().toUpperCase()) > -1) {
-				$('#elencoRicerca').append("<li onclick='scriviricerca(this)'>"+lista[i]+"</li>");
+				$('#elencoRicerca').append("<li onclick='scriviricerca(this)'>" + lista[i] + "</li>");
 			}
 		}
 	});
 }
-function elencoOfferte()
-{
-	let ilMioHtml="";
+
+function elencoOfferte() {
+	let ilMioHtml = "";
 	$(".elencoOfferte>div").hover(
-	function() {
-    ilMioHtml=$(this).html();
-		$(this).html("<p data-bs-toggle='modal' data-bs-target='#informazioni'>Informazioni</p><p data-bs-toggle='modal' data-bs-target='#applica'>Applica</p>");
-    },function() {
-        $(this).html(ilMioHtml);
-    })
+		function () {
+			ilMioHtml = $(this).html();
+			$(this).html("<p data-bs-toggle='modal' data-bs-target='#informazioni'>Informazioni</p><p data-bs-toggle='modal' data-bs-target='#applica'>Applica</p>");
+		},
+		function () {
+			$(this).html(ilMioHtml);
+		})
 }
-function scriviricerca(event)
-{
-	document.getElementById('ricercona').value=event.innerHTML;
+
+function scriviricerca(event) {
+	document.getElementById('ricercona').value = event.innerHTML;
 }
