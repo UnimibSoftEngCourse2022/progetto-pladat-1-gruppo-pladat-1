f19a71f027cad3594a5ac1c4e27b431548f37213
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2022-02-17 05:33:12 +0100

    finalizzazione controlli pagina privata

--- view/javascript/private.js
+++ view/javascript/private.js
@@ -6,18 +6,18 @@ $(document).ready(function () {
 	invioDati();
 	change();
 	invioDati2();
-	$(".elenco-privato").css('height', ($("#modifica").height()+43.2) + "px");
+	startModifica();
+	$(".elenco-privato").css('height', ($("#modifica").height() + 43.2) + "px");
 })
 
 $(window).on('resize', function () {
-	$(".elenco-privato").css('height', ($("#modifica").height()+43.2 ) + "px");
+	$(".elenco-privato").css('height', ($("#modifica").height() + 43.2) + "px");
 });
 
 function blurr() {
 	$(".gruppo > input[type=date]").focus(function () {
-		if(!$(this).is(':disabled'))
-		{
-		$(this).css("color", "#3c4043");
+		if (!$(this).is(':disabled')) {
+			$(this).css("color", "#3c4043");
 		}
 	});
 	$(".gruppo > input[type=date]").focusout(function () {
@@ -28,9 +28,8 @@ function blurr() {
 		}
 	});
 	$(".gruppo > input, .gruppo>textarea").focus(function () {
-		if(!$(this).is(':disabled'))
-		{
-		$(this).parent().addClass("gruppo-evidenziato");
+		if (!$(this).is(':disabled')) {
+			$(this).parent().addClass("gruppo-evidenziato");
 		}
 	});
 	$(".gruppo > input, .gruppo>textarea").focusout(function () {
@@ -40,8 +39,7 @@ function blurr() {
 		}
 	});
 	$(".gruppo > div>input").focus(function () {
-		if(!$(this).is(':disabled'))
-		{
+		if (!$(this).is(':disabled')) {
 			$(this).parent().parent().addClass("gruppo-evidenziato");
 		}
 	});
@@ -52,22 +50,18 @@ function blurr() {
 		}
 	});
 	$(".gruppo > label").click(function () {
-		if($(this).parent().children().get(1).tagName==="input")
-		{
-			if(!$(this).parent().children("input").is(':disabled'))
-			{
+		if ($(this).parent().children().get(1).tagName === "input") {
+			if (!$(this).parent().children("input").is(':disabled')) {
 				$(this).parent().addClass("gruppo-evidenziato");
 				$(this).parent().children("input").focus();
 			}
 		}
-		if($(this).parent().children().get(1).tagName==="textarea")
-		{
-		if(!$(this).parent().children("textarea").is(':disabled'))
-		{
-			$(this).parent().addClass("gruppo-evidenziato");
-			$(this).parent().children("textarea").focus();
+		if ($(this).parent().children().get(1).tagName === "textarea") {
+			if (!$(this).parent().children("textarea").is(':disabled')) {
+				$(this).parent().addClass("gruppo-evidenziato");
+				$(this).parent().children("textarea").focus();
+			}
 		}
-	}
 	});
 }
 
@@ -78,31 +72,35 @@ function loadMapScenario() {
 	});
 
 	function onLoad() {
-		var options = { maxResults: 5 };
+		var options = {
+			maxResults: 5
+		};
 		var manager = new Microsoft.Maps.AutosuggestManager(options);
 		manager.attachAutosuggest('#searchBox', '#searchBoxContainer', selectedSuggestion);
 	}
-	function onError(message) {
-	}
+
+	function onError(message) {}
 
 	function selectedSuggestion(suggestionResult) {
 		$('#searchBox').val(suggestionResult.formattedSuggestion);
 	}
 }
-function infoOfferte()
-{
-	let ilMioHtml="";
+
+function infoOfferte() {
+	let ilMioHtml = "";
 	$(".privato-placement").hover(
-	function() {
-    ilMioHtml=$(this).html();
-		$(this).html("<p data-bs-toggle='modal' data-bs-target='#informazioni'>Informazioni</p><p data-bs-toggle='modal' data-bs-target='#applicanti'>Applicanti</p>");
-    },function() {
-        $(this).html(ilMioHtml);
-    });
+		function () {
+			ilMioHtml = $(this).html();
+			$(this).html("<p data-bs-toggle='modal' data-bs-target='#informazioni'>Informazioni</p><p data-bs-toggle='modal' data-bs-target='#applicanti'>Applicanti</p>");
+		},
+		function () {
+			$(this).html(ilMioHtml);
+		});
 	$(".bloccoelencoapplicanti").hover(
-		function() {
-		$(this).children().eq(1).removeClass("nascondi");
-		},function() {
+		function () {
+			$(this).children().eq(1).removeClass("nascondi");
+		},
+		function () {
 			$(this).children().eq(1).addClass("nascondi");
 		});
 }
@@ -146,30 +144,34 @@ function isDescrizione(descrizione) {
 	let regex = /^.{0,200}$/;
 	return regex.test(descrizione);
 }
+
 function isTitolo(titolo) {
 	let regex = /^.{1,40}$/;
 	return regex.test(titolo);
 }
+
 function isDurata(durata) {
 	let regex = /^.{1,40}$/;
 	return regex.test(durata);
 }
+
 function isSalario(salario) {
 	let regex = /^\d+.{0,10}$/;
 	return regex.test(salario);
 }
 
-let chi="utente";
+let chi = "utente"; //quiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 function change() {
-	if (chi==="utente") {
+	if (chi === "utente") {
 		$('.gazienda').addClass("nascondi");
 		$('.gutente').removeClass("nascondi");
 	}
-	if (chi==="azienda") {
+	if (chi === "azienda") {
 		$('.gazienda').removeClass("nascondi");
 		$('.gutente').addClass("nascondi");
 	}
 }
+
 function invioDati() {
 	$("#invioDatiPrivato").parent().children(".gruppo").children("input[name='email']").get(0).setCustomValidity("L'email deve avere il formato corretto");
 	$("#invioDatiPrivato").parent().children(".gruppo").children("input[name='password']").get(0).setCustomValidity("La password deve avere tra i 6 ed i 20 caratteri, contenere almento una lettera maiuscola, almeno una lettera minuscola ed almeno un numero. NON sono concessi caratteri speciali");
@@ -184,7 +186,7 @@ function invioDati() {
 	$("#invioDatiPrivato").parent().children(".gruppo").children("textarea[name='descrizione1']").get(0).setCustomValidity("Non è richiesta una descrizione (massimo 200 caratteri)");
 	$("#invioDatiPrivato").parent().children(".gruppo").children("input[name='data']").val("");
 	$("#invioDatiPrivato").click(function () {
-		if (chi==="utente") {
+		if (chi === "utente") {
 			let email = $(this).parent().children(".gruppo").children("input[name='email']").val().trim().toLowerCase();
 			let password = $(this).parent().children(".gruppo").children("input[name='password']").val().trim();
 			let nome = $(this).parent().children(".gruppo").children("input[name='nome']").val().trim().toLowerCase();
@@ -223,7 +225,7 @@ function invioDati() {
 				$(this).parent().children(".gruppo").children("textarea[name='descrizione']").parent().children("input").css("border-color", "#1a73e8");
 			}
 		}
-		if (chi==="azienda") {
+		if (chi === "azienda") {
 			let email1 = $(this).parent().children(".gruppo").children("input[name='email1']").val().trim().toLowerCase();
 			let password1 = $(this).parent().children(".gruppo").children("input[name='password1']").val().trim();
 			let nomeCompagnia = $(this).parent().children(".gruppo").children("input[name='nomeCompagnia']").val().trim().toLowerCase();
@@ -257,8 +259,33 @@ function invioDati() {
 		}
 	});
 }
-function invioDati2()
-{
+
+function startModifica() {
+	$("#invioDatiPrivato").css("display", "none");
+	$("#modificaPrivato").click(function () {
+		if ($(this).text() === "Modifica") {
+			if (chi === "azienda") {
+				$(".gazienda").children().prop("disabled", false);
+			}
+			if (chi === "utente") {
+				$(".gutente").children().prop("disabled", false);
+			}
+			$(this).text("Annulla");
+			$("#invioDatiPrivato").css("display", "block");
+		} else {
+			if (chi === "azienda") {
+				$(".gazienda").children().prop("disabled", true);
+			}
+			if (chi === "utente") {
+				$(".gutente").children().prop("disabled", true);
+			}
+			$(this).text("Modifica");
+			$("#invioDatiPrivato").css("display", "none");
+		}
+	});
+}
+
+function invioDati2() {
 	$("#invioDatiOfferta").parent().children(".gruppo").children("input[name='titolo']").get(0).setCustomValidity("Il titolo deve avere da 1 a 40 caratteri");
 	$("#invioDatiOfferta").parent().children(".gruppo").children("input[name='durata']").get(0).setCustomValidity("La durata è obbligatoria (es: 3 settimane)");
 	$("#invioDatiOfferta").parent().children(".gruppo").children("input[name='dataInizio']").get(0).setCustomValidity("La data è obbligatoria");
@@ -299,12 +326,10 @@ function invioDati2()
 		} else {
 			$(this).parent().children(".gruppo").children("input[name='descrizione2']").parent().children("input").css("border-color", "#1a73e8");
 		}
-		if (!idData(salario)) {
+		if (!isSalario(salario)) {
 			$(this).parent().children(".gruppo").children("input[name='salario']").parent().children("input").css("border-color", "#ea4335");
 		} else {
 			$(this).parent().children(".gruppo").children("input[name='salario']").parent().children("input").css("border-color", "#1a73e8");
 		}
 	});
 };
-
-
