ec7346b24a30a4dcb4a067c3ffb78deb881e815e
Matteo Severgnini <severgnini.matteo.00@gmail.com>
2022-02-19 16:52:55 +0100

    Modifica del model Student

---
+++ PlaDat/app/Models/Req.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Req extends Model
+{
+    use HasFactory;
+
+    protected $table = 'request';
+    protected $primaryKey = ['student_email', 'idPlacement'];
+    protected $keyType = 'integer';
+    protected $connection = 'mysql';
+
+    protected $fillable = [
+        'student_email',
+        'presentation_letter',
+        'idPlacement',
+    ];
+
+    protected function setKeysForSaveQuery($query)
+    {
+        $query
+            ->where('student_email', '=', $this->getAttribute('student_email'))
+            ->where('idPlacement', '=', $this->getAttribute('idPlacement'));
+
+        return $query;
+    }
+
+
+}
